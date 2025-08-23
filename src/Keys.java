bc2a0b553600ced47ef27f283f822a25d2562c9a
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 19:50:51 -0300

    tabela condicional

--- src/Keys.java
+++ src/Keys.java
@@ -3,16 +3,39 @@ import java.util.Scanner;
 public class Keys {
     public static void main(String[] args){
 
-        var scanner = new Scanner(System.in);
 
-        System.out.println("Quantos anos vc tem?");
-        var age = scanner.nextInt();
 
-        System.out.println("Vc é emancipado??");
-        var isEmancipated = scanner.nextBoolean();
-        var canDrive = age >= 18  || (isEmancipated && age >= 16); // ser usado apenas (isEmancipated == true)
+        System.out.println("TABELA CONDICIONAL");
+        System.out.println("==================================");
 
-        System.out.printf("Vc pode dirigir? %s \n", canDrive);
+        System.out.printf("true   &&  true  =  %s \n", true && true);
+        System.out.printf("false  &&  false = %s \n", false && false);
+        System.out.printf("true   &&  false = %s \n", true && false);
+        System.out.printf("false  &&  true  =  %s \n", false && true);
+
+        System.out.println("==================================");
+
+        System.out.printf("true   ||  false = %s \n", true || true);
+        System.out.printf("false  ||  false = %s \n", false || false);
+        System.out.printf("true   ||  false = %s \n", true || false);
+        System.out.printf("false  ||  false = %s \n", false || true);
+
+        System.out.println("==================================");
+
+        System.out.printf("!true  = %s \n", !true);
+        System.out.printf("!false = %s \n", !false);
+
+
+//        var scanner = new Scanner(System.in);
+//
+//        System.out.println("Quantos anos vc tem?");
+//        var age = scanner.nextInt();
+//
+//        System.out.println("Vc é emancipado??");
+//        var isEmancipated = scanner.nextBoolean();
+//        var canDrive = age >= 18  || (isEmancipated == true && age >= 16); // pode ser usado apenas (isEmancipated)
+//
+//        System.out.printf("Vc pode dirigir? %s \n", canDrive);
 
 
 
