578046b738d881b8f76131a8565c04b277121994
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 17:38:53 -0300

    exemplos de tipos primitivos e usos básicos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
+
+    //variavel global, (boa prática declarar em maiúsculas e underline)
+private final static  String WELCOME_MSG = "Olá, qual o seu nome?";
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(WELCOME_MSG);
+        String name = scanner.next();
+
+        System.out.println("Qual sua idade?");
+        int age = scanner.nextInt();
+
+        System.out.printf("Oi %s, vc tem %s anos!!", name, age);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("seu nome");
+//        String name = scanner.next();
+//
+//        System.out.println("sua idade");
+//        int age = scanner.nextInt();
+//
+//        System.out.printf("oi %s, vc tem %s anos", name, age);
+
+
+//forma um pouco reduzida de declaração de variaveis
+
+//    public static void main(String[] args) {
+//        var scanner = new Scanner(System.in);
+//        System.out.println("seu nome");
+//        var name = scanner.next();
+//        System.out.println("sua idade");
+//        var age = scanner.nextInt();
+//        System.out.printf("oi %s, vc tem %s anos", name, age);
+//    }
     }
 }
