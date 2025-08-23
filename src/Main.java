6fc1053da1f42402b186b9ff28820842d34eb059
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 18:09:30 -0300

    tipos primitivos

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,46 @@ import java.util.Scanner;
 
 public class Main {
 
-    //variavel global, (boa prática declarar em maiúsculas e underline)
-private final static  String WELCOME_MSG = "Olá, qual o seu nome?";
-
     public static void main(String[] args) {
 
+    //tipos primitivos
+        byte baite = 1;
+        short chorte = 1;
+        int inteiro = 1;
+        long longo = 1L;
+        float decimal = 1.0f;
+        double decimal2 = 1.0d;
+        char caractere = '1'; //aspas simples
+        boolean boleano = true; //ou 'false'
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                " byte: " + baite + ",\n " +
+                "short: " + chorte + ",\n " +
+                "int: " + inteiro + ",\n " +
+                "long: " + longo + ",\n " +
+                "float: " + decimal + ",\n " +
+                "double: " + decimal2 + ",\n " +
+                "char: " + caractere + ",\n " +
+                "boolean: "+ boleano
+        );
 
-        System.out.println(WELCOME_MSG);
-        String name = scanner.next();
 
-        System.out.println("Qual sua idade?");
-        int age = scanner.nextInt();
 
-        System.out.printf("Oi %s, vc tem %s anos!!", name, age);
+        //variavel global, (boa prática declarar em maiúsculas e underline)
+//private final static  String WELCOME_MSG = "Olá, qual o seu nome?";
+//
+//    public static void main(String[] args) {
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(WELCOME_MSG);
+//        String name = scanner.next();
+//
+//        System.out.println("Qual sua idade?");
+//        int age = scanner.nextInt();
+//
+//        System.out.printf("Oi %s, vc tem %s anos!!", name, age);
 
 
 //        Scanner scanner = new Scanner(System.in);
