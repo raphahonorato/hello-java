3293cacfc397687df4e15c0b02a69f48c3785c42
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 19:27:50 -0300

    mais operadores condicionais

--- src/Keys.java
+++ src/Keys.java
@@ -2,21 +2,37 @@ import java.util.Scanner;
 
 public class Keys {
     public static void main(String[] args){
+
         var scanner = new Scanner(System.in);
 
-        System.out.println("quanto é 2 + 2?");
+        System.out.println("Quantos anos vc tem?");
+        var age = scanner.nextInt();
 
-        var result = scanner.nextInt();
-        var isRight = result == 4;
+        System.out.println("Vc é emancipado??");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18  || (isEmancipated && age >= 16); // ser usado apenas (isEmancipated == true)
 
-        if(isRight){
+        System.out.printf("Vc pode dirigir? %s \n", canDrive);
 
-            System.out.println("resposta é 4, vc acertou!");
-        }
 
-        else{
-            System.out.printf("Vc disse que é %d, mas a resposta é 4, vc errou ", result);
-        }
+
+
+
+//        var scanner = new Scanner(System.in);
+//
+//        System.out.println("quanto é 2 + 2?");
+//
+//        var result = scanner.nextInt();
+//        var isRight = result == 4;
+//
+//        if(isRight){
+//
+//            System.out.println("resposta é 4, vc acertou!");
+//        }
+//
+//        else{
+//            System.out.printf("Vc disse que é %d, mas a resposta é 4, vc errou ", result);
+//        }
 
     }
 }
