15728a1d66bc252f486d32148e9dab74333c1c1d
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 21:33:49 -0300

    Operadores Bitwise

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        var value1 = 4;
+        var binary1 = Integer.toBinaryString(value1);
+        System.out.printf("primeiro numero %s (representação binaria) %s \n", value1, binary1);
 
+        var value2 = 5;
+        var binary2 = Integer.toBinaryString(value2);
+        System.out.printf("segundo numero %s (representação binaria) %s\n",value2,binary2);
 
-        var value = (1 + 2) * 5 / 2;
-        System.out.println(value);
-
-//        var scanner = new Scanner(System.in);
-//
-//        System.out.println("Informe o primeiro numero:");
-//        int value1 = scanner.nextInt();
-//
-//        System.out.println("Informe o primeiro numero:");
-//        int value2 = scanner.nextInt();
-//
-////        int some = value1 + value2;
-////        System.out.println(value1 + " + " + value2 +  " = " + value1 + value2); // wrong method
-//        System.out.printf("%s + %s = %s",value1 , value2, value1 + value2);
+        var result = value1 & value2;
+        var binaryResult = Integer.toBinaryString(result);
+        System.out.printf("%s | %s = %s  (representação binaria %s)\n", value1, value2, result, binaryResult);
     }
 }
+
+/*
+
+0 = true
+1 = false
+
+ |   sempre considera o true
+
+
+
+
+*/
