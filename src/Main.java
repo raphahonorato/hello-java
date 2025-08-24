5b91a0a6b2f69b64640140e171d5380cc4ae875e
Raphael Honorato <raphaahonorato@gmail.com>
2025-08-23 21:03:12 -0300

    operadores aritméticos

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
-    //tipos primitivos
-        byte baite = 1;
-        short chorte = 1;
-        int inteiro = 1;
-        long longo = 1L;
-        float decimal = 1.0f;
-        double decimal2 = 1.0d;
-        char caractere = '1'; //aspas simples
-        boolean boleano = true; //ou 'false'
 
-        System.out.println(
-                " byte: " + baite + ",\n " +
-                "short: " + chorte + ",\n " +
-                "int: " + inteiro + ",\n " +
-                "long: " + longo + ",\n " +
-                "float: " + decimal + ",\n " +
-                "double: " + decimal2 + ",\n " +
-                "char: " + caractere + ",\n " +
-                "boolean: "+ boleano
-        );
+        var value = (1 + 2) * 5 / 2;
+        System.out.println(value);
 
-
-
-        //variavel global, (boa prática declarar em maiúsculas e underline)
-//private final static  String WELCOME_MSG = "Olá, qual o seu nome?";
-//
-//    public static void main(String[] args) {
-//
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println(WELCOME_MSG);
-//        String name = scanner.next();
-//
-//        System.out.println("Qual sua idade?");
-//        int age = scanner.nextInt();
-//
-//        System.out.printf("Oi %s, vc tem %s anos!!", name, age);
-
-
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("seu nome");
-//        String name = scanner.next();
-//
-//        System.out.println("sua idade");
-//        int age = scanner.nextInt();
-//
-//        System.out.printf("oi %s, vc tem %s anos", name, age);
-
-
-//forma um pouco reduzida de declaração de variaveis
-
-//    public static void main(String[] args) {
 //        var scanner = new Scanner(System.in);
-//        System.out.println("seu nome");
-//        var name = scanner.next();
-//        System.out.println("sua idade");
-//        var age = scanner.nextInt();
-//        System.out.printf("oi %s, vc tem %s anos", name, age);
-//    }
+//
+//        System.out.println("Informe o primeiro numero:");
+//        int value1 = scanner.nextInt();
+//
+//        System.out.println("Informe o primeiro numero:");
+//        int value2 = scanner.nextInt();
+//
+////        int some = value1 + value2;
+////        System.out.println(value1 + " + " + value2 +  " = " + value1 + value2); // wrong method
+//        System.out.printf("%s + %s = %s",value1 , value2, value1 + value2);
     }
 }
