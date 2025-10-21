729fef0f1700aabebed7c9cb8aaa6d2139ab791b
Raphael Honorato <raphaahonorato@gmail.com>
2025-10-21 15:12:38 -0300

    mais exercicios

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,82 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args){
 
-        var scanner = new Scanner(System.in);
+    var scanner = new Scanner(System.in);
 
-        System.out.println("Qual seu nome?");
-        var name = scanner.next();
+        System.out.println("Digite um nome: ");
+        var nome1 = scanner.next();
 
-        System.out.println("Quantos anos vc tem?");
-        var age = scanner.nextInt();
+        System.out.println("Digite uma idade: ");
+        var idade1 = scanner.nextInt();
 
-        System.out.println("vc é emancipado?");
-        var isEmancipated = scanner.nextBoolean();
 
-        if (age >= 18){
-            System.out.printf("olá %s vc tem %s, vc pode dirigir :D", name, age);
-        } else if (isEmancipated && age < 18 && age > 15) {
-            System.out.printf("olá %s vc tem %s, vc pode dirigir pq é emancipado :D", name, age);
-        } else{
-            System.out.printf("%s com %s, vc não pode dirigir :(", name, age);
+        System.out.println("Digite outro nome: ");
+        var nome2 = scanner.next();
+
+        System.out.println("Digite outra idade: ");
+        var idade2 = scanner.nextInt();
+
+        var diferencaIdade = Math.abs(idade1 - idade2);
+
+        if(idade1 > idade2){
+            System.out.println(nome1 + " é " + diferencaIdade + " anos mais velho(a) que " + nome2 + ".");
+        }else if(idade1 < idade2){
+            System.out.println(nome2 + " é " + diferencaIdade + " anos mais velho(a) que " + nome1 + ".");
+        }else{
+            System.out.println(nome1 + " e " + nome2 + " tem a mesma idade!");
         }
 
 
+
+
+
+//      ====================================================
+
+
+//    calculo area de um retangulo
+//
+//    var scanner = new Scanner(System.in);
+//
+//    System.out.println("Informe a base do retângulo: ");
+//    int base = scanner.nextInt();
+//
+//    System.out.println("Informe a altura do retângulo: ");
+//    int altura = scanner.nextInt();
+//
+//    int areaRetang = base * altura;
+//
+//    System.out.println("A area do retângulo é: " + areaRetang + " m²");
+
+
+
+//      ====================================================
+
+//        calculo de area de um quadrado
+//
+//        var scanner = new Scanner(System.in);
+//
+//        System.out.println("Informe o valor do lado de um quadrado:");
+//
+//        int lado = scanner.nextInt();
+//        int area = lado * lado;
+//
+//        System.out.println("A area do quadrado é: " + area + " m²");
+
+
+//      ====================================================
+
+//        exercicio calculo de idade
+
+//        var scanner = new Scanner(System.in);
+//        System.out.println("Qual seu nome?");
+//        var nome = scanner.next();
+//
+//        System.out.println("Qual seu ano de nascimento?");
+//        var nascimento = scanner.nextInt();
+//
+//        int idade = 2025 - nascimento;
+//        System.out.println("Olá " + nome + " você tem " + idade + " anos!");
     }
 }
