4f02437ca170c3668f08086f418313d1a78d5fea
Raphael Honorato <raphaahonorato@gmail.com>
2025-10-22 17:37:38 -0300

    outra forma de implementar switch (como atribuição)

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,17 @@ public class Main {
 
             var option = scanner.nextInt();
 
-            switch (option) {
-                case 1, 7 -> System.out.println("Fim de semana !!");
-                case 2 -> System.out.println("Segunda");
-                case 3 -> System.out.println("Terça");
-                case 4 -> System.out.println("Quarta");
-                case 5 -> System.out.println("Quinta");
-                case 6 -> System.out.println("Sexta");
+            var message = switch (option) {
+                case 1, 7 -> "Fim de semana !!";
+                case 2 -> "Segunda";
+                case 3 -> "Terça";
+                case 4 -> "Quarta";
+                case 5 -> "Quinta";
+                case 6 -> "Sexta";
 //                case 7 -> System.out.println("Sábado");
-                default -> System.out.println("número inválido!");
-            }
+                default -> "número inválido!";
+            };
+            System.out.println(message);
         } else {
             System.out.println("Não é um numero!");
         }
