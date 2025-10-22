80aa7f66e6e49c55d474daadace7a3d0548c8ac8
Raphael Honorato <raphaahonorato@gmail.com>
2025-10-22 20:29:50 -0300

    estruturas de repetição

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        var scanner = new Scanner(System.in);
-
-
-//      estrutura de controle switch
-        System.out.println("informe um numero de 1 a 7: ");
-
-        if (scanner.hasNextInt()) {
-
-            var option = scanner.nextInt();
-
-            var message = switch (option) {
-                case 1, 7 -> {
-                    String day = option == 1 ? "Domingo" : "Sábado";
-                    yield  String.format("Hoje é %s", day);
-                }
-                case 2 -> "Segunda";
-                case 3 -> "Terça";
-                case 4 -> "Quarta";
-                case 5 -> "Quinta";
-                case 6 -> "Sexta";
-//                case 7 -> System.out.println("Sábado");
-                default -> "número inválido!";
-            };
-            System.out.println(message);
-        } else {
-            System.out.println("Não é um numero!");
+//        diferença entre while / do while
+        var i = 0;
+        while (args.length > i) {
+            System.out.println(args[i]);
+            i++;
         }
 
-//      ====================================================
-//
-//
-//        condicionais if else
-//
+        i = 0;
+
+        System.out.println("=================");
+
+        do {
+            System.out.println(args[i]);
+            i++;
+        }
+        while (args.length > i);
+
+
+//        outras aplicações de while
 //        var scanner = new Scanner(System.in);
+//        var name = "";
 //
-//        System.out.println("informe seu nome: ");
-//        String name = scanner.next();
+//        while (true) {
+//            System.out.println("informe um nome: ");
+//            name = scanner.next();
 //
-//        System.out.println("informe sua idade: ");
-//        int age = scanner.nextInt();
+//            System.out.println(name);
 //
-//        if (age >= 18) System.out.printf("Olá %s você tem %s anos, já pode dirigir!", name, age);
-//        else System.out.printf("%s, você ainda é menor de idade, não pode dirigir!", name);
+//            if(name.equalsIgnoreCase("exit")) break;
+//        }
+
+
+//        laço de repetição while
+
+//        var scanner = new Scanner(System.in);
+//        var name = "";
+//
+//
+//        while(!name.equals( "sair")){
+//
+//            System.out.println("Digite um nome");
+//            name = scanner.next();
+//
+//            System.out.println("olá, " + name);
+//
+//        }
+//    System.out.println("Saiu");
+
+//        lanços de repetição for
+
+//        for(var i = 2; i <= 100; i++ ){
+//            System.out.println(i);
+//        }
     }
 }
